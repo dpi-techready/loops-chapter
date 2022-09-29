909824939f85f9fdf6b02a1f6b1fdc2a57ddc4df
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-09-29 15:53:27 +0000

    completed

--- loops_fizz_buzz.rb
+++ loops_fizz_buzz.rb
@@ -5,9 +5,13 @@
 #   for multiples of five print "Buzz" instead of the number
 #   for numbers which are multiples of both three and five print "FizzBuzz"
 
-i = 0
-
-# loop do 
-#   i = i + 1
-#   puts i
-#   end
+for i in 0..100
+if i % 3 == 0 && i % 5 == 0
+  p "FizzBuzz"
+elsif i % 3 == 0
+    p "Fizz"
+  elsif i % 5 == 0
+    p "Buzz"
+else p i
+  end
+end  
